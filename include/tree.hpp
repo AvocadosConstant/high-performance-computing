b8aac328e46f9e4bf571faaf1a0a0b94590736d1
Tim Hung <thung1@binghamton.edu>
2018-02-26 14:59:51 -0500

    Edit some aliases

--- include/tree.hpp
+++ include/tree.hpp
@@ -7,8 +7,11 @@
 #include <random>
 
 
-typedef std::vector<std::vector<float>> points_t;
-typedef std::vector<int> i_vec_t;
+using points_t = std::vector<std::vector<float>>;
+using int_vec = std::vector<int>;
+
+const int MIN_SAMPLE_SIZE = 100;
+
 
 struct Node {
   Node() :
@@ -26,36 +29,38 @@ struct Node {
   int level_;
 };
 
+
 struct Job {
   Node *parent;
-  i_vec_t indices;
+  int_vec indices;
 };
+
 
 class KDTree {
   public:
     KDTree(points_t &p, int d) : points_(p), dims_(d) {
 
-      i_vec_t indices(points_.size());
+      int_vec indices(points_.size());
       std::iota(indices.begin(), indices.end(), 0);
 
       int median = sample_median_index(indices, 0);
       std::cout << "Sample median:\t" << "p[" << median << "] = " << points_[median][0] << std::endl;
 
-      i_vec_t left, right;
+      int_vec left, right;
       std::tie(left, right) = split(median, indices, 0);
 
-      assert(points_.size() == left.size() + right.size() + 1);
     }
+
 
   private:
     Node *root_;
     points_t &points_;
     int dims_;
 
-    int sample_median_index(const i_vec_t &indices, const int dim) {
-      i_vec_t sample;
+    int sample_median_index(const int_vec &indices, const int dim) {
+      int_vec sample;
       std::sample(indices.begin(), indices.end(),
-          std::back_inserter(sample), std::min(80, (int)indices.size()),
+          std::back_inserter(sample), std::min(MIN_SAMPLE_SIZE, (int)indices.size()),
           std::mt19937{std::random_device{}()});
 
       std::sort(sample.begin(), sample.end(), [&](int a, int b) {
@@ -66,9 +71,9 @@ class KDTree {
     }
 
 
-    std::pair<i_vec_t, i_vec_t> split(int pivot_i, i_vec_t &indices, int dim) {
-      i_vec_t left;
-      i_vec_t right;
+    std::pair<int_vec, int_vec> split(int pivot_i, int_vec &indices, int dim) {
+      int_vec left;
+      int_vec right;
       float pivot = points_[pivot_i][dim];
 
       for (auto i : indices) {
@@ -76,7 +81,8 @@ class KDTree {
         if (cur < pivot) left.push_back(i);
         else if (cur > pivot) right.push_back(i);
       }
-      return std::pair<i_vec_t, i_vec_t>(left, right);
+      assert(indices.size() == left.size() + right.size() + 1);
+      return std::pair<int_vec, int_vec>(left, right);
     }
 
 
