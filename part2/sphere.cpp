707dfc93ac163a94e41e275ab828f838f940cd54
Tim Hung <thung1@binghamton.edu>
2018-03-29 15:19:41 -0500

    Implement histogram printing and add function

--- part2/sphere.cpp
+++ part2/sphere.cpp
@@ -8,11 +8,14 @@
 #include <array>
 #include <assert.h>
 #include <cmath>
+#include <iomanip>
 
-const int N = 10;
+//const int N = 10;
+const int N = 100'000;
 //const int N = 100'000'000;
 const int MIN_DIM = 2;
 const int MAX_DIM = 16;
+const int HIST_BUCKETS = 10;
 
 double time(const std::function<void ()> &f) {
   f(); // Run once to warmup.
@@ -23,19 +26,16 @@ double time(const std::function<void ()> &f) {
   return std::chrono::duration<double>(stop - start).count();
 }
 
-int main() {
-
-  std::vector<std::vector<float>> hists(MAX_DIM - MIN_DIM);
+std::vector<std::vector<int>> generate_histograms(bool parallel) {
+  std::vector<std::vector<int>> hists;
   std::vector<std::vector<float>> points(N, std::vector<float>(MAX_DIM + 2));
   std::vector<float> dists(N);
-
+  std::vector<int> hist(HIST_BUCKETS);
 
   std::minstd_rand eng;
   std::uniform_real_distribution<float> dist(-1, 1);
 
   for (int dims = 2; dims <= 16; dims++) {
-    std::cout << "\n\t" << dims << "-D" << std::endl;
-
     std::fill(dists.begin(), dists.end(), 0);
 
     for (std::size_t i = 0; i < points.size(); i++) {
@@ -68,9 +68,26 @@ int main() {
     }
     // dists should consist of distances of the N uniform points on thedims-ball
 
+    std::fill(hist.begin(), hist.end(), 0);
     // Histogram and stuff and graphs
     for (auto dist : dists) {
-      std::cout << dist << std::endl;
+      hist[(int) (dist * hist.size())] += 1;
+    }
+
+    hists.push_back(hist);
+  }
+  return hists;
+}
+
+int main() {
+  auto hists = generate_histograms(true);
+
+  for (int dims = 2; dims <= 16; dims++) {
+    std::cout << "\n\t" << dims << "-D" << std::endl;
+    std::cout << "Histogram with " << HIST_BUCKETS << " buckets" << std::endl;
+    for (size_t i = 0; i < hists[dims-2].size(); i++) {
+      std::cout.precision(2);
+      std::cout << std::fixed << std::setw(2) << ((float) i) / hists[dims-2].size() << "\t\t" << (int)(100.0 * hists[dims-2][i] / N) << "%" << std::endl;
     }
   }
 }
